bb2ff5461fc33225a5e6eee969a98fcd4f867562
maxazimi <maxazimy@gmail.com>
2024-10-13 00:39:56 +0330

    page navigation refactored

--- values_mobile.go
+++ values_mobile.go
@@ -7,8 +7,25 @@ import (
 )
 
 const (
-	WindowWidth  = unit.Dp(360)
-	WindowHeight = unit.Dp(780)
+	AppWidth  = unit.Dp(360)
+	AppHeight = unit.Dp(780)
+
+	DP16  = unit.Dp(12)
+	DP18  = unit.Dp(12)
+	DP24  = unit.Dp(16)
+	DP30  = unit.Dp(16)
+	DP32  = unit.Dp(16)
+	DP420 = unit.Dp(340)
+
+	TextSize18 = unit.Sp(16)
+	TextSize20 = unit.Sp(18)
+	TextSize22 = unit.Sp(18)
+	TextSize24 = unit.Sp(20)
+	TextSize28 = unit.Sp(22)
+	TextSize30 = unit.Sp(24)
+	TextSize32 = unit.Sp(28)
+	TextSize34 = unit.Sp(28)
+	TextSize60 = unit.Sp(34)
 
 	ModalRadius = unit.Dp(25)
 )
