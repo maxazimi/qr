e1f5a6b7a5457fe2b0fd39ef145417e1743fede9
maxazimi <maxazimy@gmail.com>
2024-12-31 01:25:06 +0330

    events added

---
+++ ui/ev/events.go
@@ -0,0 +1,38 @@
+package ev
+
+type Event interface{}
+
+type WindowSizeEvent struct {
+	Width, Height int
+}
+
+type AddModalEvent struct {
+	Modal interface{}
+}
+
+type QREvent struct{}
+
+type ExitEvent struct{}
+
+type FatalEvent struct {
+	Err error
+}
+
+type NotifyEvent struct {
+	Level int
+	Text  string
+}
+
+var (
+	uiEvent = make(chan Event)
+)
+
+func RequestEvent(e Event) {
+	go func() {
+		uiEvent <- e
+	}()
+}
+
+func Events() <-chan Event {
+	return uiEvent
+}
